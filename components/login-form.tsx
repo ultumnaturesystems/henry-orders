e87ab7924f8573c64cc3a97c6352cc46ad5ab5d2
Derek Le <derekle1105@gmail.com>
2025-06-23 15:22:49 -0700

    utilized route groups to seperate authentication routes from rendering navbar

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { login } from "../app/login/actions";
+import { login } from "../app/(auth)/login/actions";
 import { Eye, EyeOff } from "lucide-react";
 
 import { useState } from "react";
