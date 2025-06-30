d358c14f74a2ce5a51f4b3ee87da5a35df74dc50
Derek Le <derekle1105@gmail.com>
2025-06-30 08:29:25 -0700

    refactor: simplify RootLayout by removing unused font imports and metadata

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,23 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import NavigationMenu from "@/components/navigation-menu";
 import NextTopLoader from "nextjs-toploader";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "UNS Orders",
-  description: "Shopify Orders for UNS",
-};
 
 export default function RootLayout({
   children,
@@ -25,15 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-        style={{ background: "rgba(241, 241, 241, 1)" }}
-      >
-        <NextTopLoader />
-        <NavigationMenu />
-        {children}
-      </body>
-    </html>
+    <>
+      <NextTopLoader />
+      <NavigationMenu />
+      {children}
+    </>
   );
 }
